8225176c41d181236e5dbdefe7174a2d839d5ed7
Erik Steinfeld <eriks@attackpattern.com>
2017-01-26 17:07:53 -0800

    try a different port

--- config/app.js
+++ config/app.js
@@ -9,7 +9,7 @@
  */
 module.exports = Object.freeze({
   app: {
-    port: process.env.APP_PORT || '80'
+    port: process.env.APP_PORT || '8080'
   },
   assets: {
     path: process.env.ASSETS_PATH || '/public',
